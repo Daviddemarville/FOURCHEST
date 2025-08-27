2c0ca8d7521416111c92a2f450be696acdb81c94
David de Marville <de.marville.david@gmail.com>
2025-08-27 17:28:09 +0200

    add vite.config for route

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// vite.config.js
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
-	base: "/", // Replace 'my-repo' with your actual repository name
-	plugins: [tailwindcss()],
+  // base "/" OK sur Vercel (utile surtout pour GitHub Pages)
+  base: "/",
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index:       resolve(__dirname, "index.html"),
+        about:       resolve(__dirname, "about.html"),
+        account:     resolve(__dirname, "account.html"),
+        restaurant:  resolve(__dirname, "restaurant.html"),
+        order:       resolve(__dirname, "order.html"),
+        finalOrder:  resolve(__dirname, "final-order.html"),
+        log:         resolve(__dirname, "log.html"),
+      },
+    },
+  },
 });
